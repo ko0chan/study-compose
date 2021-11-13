c9683395ce4dc48b22dfeb87c7975e34826c2218
ko0chan <kychan1320@gmail.com>
2021-11-13 22:59:47 +0900

    13. 마무리 터치!

--- app/src/main/java/com/biggates/studycompose/ui/theme/Theme.kt
+++ app/src/main/java/com/biggates/studycompose/ui/theme/Theme.kt
@@ -24,7 +24,7 @@ private val LightColorPalette = lightColors(
 
 
 @Composable
-fun StudyComposeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun BasicsCodelabTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
